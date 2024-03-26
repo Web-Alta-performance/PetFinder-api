fe1e8b620a45e63fa1ccfd75e250b6c7e332fb08
MarcioSobel <marcinhosobel@hotmail.com>
2024-03-25 21:50:23 -0300

    Add user findById

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -23,4 +23,11 @@ export class InMemoryUsersRepository implements UsersRepository {
         if (!user) return null;
         return user;
     }
+
+    async findById(id: string): Promise<User | null> {
+        const user = this.items.find((item) => item.id === id);
+        
+        if (!user) return null;
+        return user;
+    }
 }
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,15 +1,20 @@
 import { prisma } from '@/lib/prisma';
-import { Prisma } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 import { UsersRepository } from '../users-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
-    async create(data: Prisma.UserCreateInput) {
+    async create(data: Prisma.UserCreateInput): Promise<User> {
         const user = await prisma.user.create({ data });
         return user;
     }
 
-    async findByEmail(email: string) {
+    async findByEmail(email: string): Promise<User | null> {
         const user = await prisma.user.findUnique({ where: { email }});
+        return user;
+    }
+
+    async findById(id: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({ where: { id }});
         return user;
     }
 }
