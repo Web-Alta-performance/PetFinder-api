8aa226a5bfcd14adbe7559fae11ce09c04f3a2f1
MarcioSobel <marcinhosobel@hotmail.com>
2024-03-31 16:03:57 -0300

    Add routes

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,9 +1,13 @@
-import { Router } from "express";
-import { authenticate } from "./authenticate";
-import { create } from "./create";
+import { Router } from 'express';
+import { authenticate } from './authenticate';
+import { create } from './create';
+import { get } from './get';
+import { fetch } from './fetch'
 
 const userRoutes = Router();
 
+userRoutes.get('/:userId', get);
+userRoutes.get('/', fetch);
 userRoutes.post('/', create);
 userRoutes.post('/auth', authenticate);
 
