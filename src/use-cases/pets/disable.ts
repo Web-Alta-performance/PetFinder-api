d54dc0f3689f7d473bb874fc6771a90e9e96edcd
MarcioSobel <marcinhosobel@hotmail.com>
2024-03-31 17:24:55 -0300

    Make disable return void

--- src/use-cases/pets/disable.ts
+++ src/use-cases/pets/disable.ts
@@ -2,19 +2,12 @@ import { PetsRepository } from "@/repositories/pets-repository";
 import { Pet } from "@prisma/client";
 import { NotFoundError } from "../errors/not-found-error";
 
-interface PetDisableUseCaseResponse {
-    pet: Pet | null
-}
 
 export class PetDisableUseCase {
     constructor(private petRepository: PetsRepository) {}
 
-    async execute(petId: string): Promise<PetDisableUseCaseResponse> {
+    async execute(petId: string): Promise<void> {
         const pet = await this.petRepository.disable(petId);
         if (!pet) throw new NotFoundError();
-        
-        return {
-            pet
-        };
     }
 }
