9696ba1cb89fbe12e64b0af875610fa650aaaa91
marciosobel <marcinhosobel@hotmail.com>
2024-04-08 08:46:53 -0300

    Show current running port in terminal

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { env } from './env';
 import { app } from './app';
 
 const connect = async () => {
-    app.listen(env.PORT, () => console.log('HTTP server running.'));
+    app.listen(env.PORT, () => console.log(`HTTP server running on port ${env.PORT}.`));
 };
 
 connect();
