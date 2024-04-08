58eb67b2ab16f2255f1ec2af2577388efd00e1f3
marciosobel <marcinhosobel@hotmail.com>
2024-04-08 08:48:03 -0300

    make auth create a new jwt token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -1,6 +1,8 @@
+import { env } from '@/env';
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error';
 import { makeUserAuthenticateUseCase } from '@/use-cases/users/factories/make-authenticate-use-case';
 import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { z } from 'zod';
 
 export async function authenticate(request: Request, response: Response, next: NextFunction) {
@@ -8,15 +10,19 @@ export async function authenticate(request: Request, response: Response, next: N
         email: z.string().email(),
         password: z.string()
     });
-    
+
     const registerUseCase = makeUserAuthenticateUseCase();
-    
+
     try {
         const { email, password } = registerBodySchema.parse(request.body);
 
-        await registerUseCase.execute({ email, password });
-        
-        return response.status(204).send();
+        const { user } = await registerUseCase.execute({ email, password });
+
+        const token = jwt.sign({
+            id: user.id
+        }, env.SECRET_TOKEN);
+
+        return response.status(200).send({ user, token });
     } catch (error) {
         if (error instanceof InvalidCredentialsError) {
             return response.status(400).send({ message: error.message });
