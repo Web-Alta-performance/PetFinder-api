8a422c3e35e2438ca40d16c142aa9e6746c99364
marciosobel <marcinhosobel@hotmail.com>
2024-04-15 02:24:23 -0300

    add update function to users repository

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,9 +1,16 @@
 import { Prisma, User } from '@prisma/client';
 
+export interface UserUpdateParams {
+    password_hash?: string,
+    name?: string,
+    phone_number?: string
+}
+
 export interface UsersRepository {
     create(data: Prisma.UserCreateInput): Promise<User>
     remove(userId: string): Promise<boolean>
     fetch(): Promise<User[]>
     findById(id: string): Promise<User | null>
     findByEmail(email: string): Promise<User | null>
+    update(email: string, data: UserUpdateParams): Promise<User | null>
 }
