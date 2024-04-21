f1090f5b795c23e5a6e5fae41cf55e38efeb3c70
marciosobel <marcinhosobel@hotmail.com>
2024-04-21 06:45:26 -0300

    change error

--- src/http/controllers/users/update.ts
+++ src/http/controllers/users/update.ts
@@ -1,4 +1,4 @@
-import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+import { NotFoundError } from "@/use-cases/errors/not-found-error";
 import { makeUserUpdateUseCase } from "@/use-cases/users/factories/make-update-use-case";
 import { Request, Response, NextFunction } from 'express';
 import { z } from 'zod';
@@ -24,8 +24,8 @@ export async function update(request: Request, response: Response, next: NextFun
 
         return response.status(200).send(user);
     } catch (error) {
-        if (error instanceof UserAlreadyExistsError) {
-            return response.status(401).send({ message: error.message });
+        if (error instanceof NotFoundError) {
+            return response.status(404).send({ message: error.message });
         }
 
         next(error);
